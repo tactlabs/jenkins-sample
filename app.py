e5e9b51d71c328eaa40bb7c4d361c136370192b0
Ubuntu <ubuntu@ip-172-31-82-181.ec2.internal>
2022-03-08 17:02:01 +0000

    changed the return statement

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'jenkins deploymemt'
+    return 'hello jenkins deploymemt'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
