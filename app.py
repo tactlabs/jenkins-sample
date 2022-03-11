61efa2b39e9723c10183e724c2e6e215edcdd267
elakia <vmelakia@gmail.com>
2022-03-11 21:06:26 +0530

    changed the requirement

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return 'hello world jenkins deployment work paniruchu !!!!'
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host='0.0.0.0')
