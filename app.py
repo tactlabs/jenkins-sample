f80f966d2dd1a1d014e4e5783bc37f783fdccce0
elakia <vmelakia@gmail.com>
2022-03-13 08:01:17 +0530

    changed the return statment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'snekha is super  !!'
+    return 'snekha is super !! !!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
