ad890a65464aaee9125c6a78483e1df829a2405e
elakia <vmelakia@gmail.com>
2022-03-14 21:55:27 +0530

    changed the return statment

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return 'hello world jenkins worked successfully !!'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5000)
