bfabb7e5503fa71d92f5cf6225b1eb14b42a5524
elakia <vmelakia@gmail.com>
2022-03-14 20:20:10 +0530

    changed the return statment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,4 @@ def hello_world():
     return 'nalla work akuthu super !!!!'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5000)
