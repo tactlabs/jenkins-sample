a1a456e3f49bcf356acf153c7c8ff91e96ab04b3
elakia <vmelakia@gmail.com>
2022-03-19 10:50:06 +0530

    changed the return statment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world jenkins worked successfully !!'
+    return 'hello world jenkins worked successfully suseendra !!!!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
